7be36ee88253f8e06809331e975f2db3665c9076
Полина Козлова <polinak87@mail.ru>
2020-01-05 16:46:27 +0600

    refactoring #27

--- frontend/src/components/Card/Content.js
+++ frontend/src/components/Card/Content.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classNames from 'classnames';
 
 export const full = '-is-full';
 
-export default function Content({children, full}) {
-  return <div className={`card-content${full}`}>{children}</div>
+export default function Content({children, className}) {
+  const classes = () => {
+    return classNames('card-content', className);
+  };
+
+  return <div className={classes()}>{children}</div>
 } 
